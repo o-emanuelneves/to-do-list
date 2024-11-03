d572495524bf50248b33caee51a034b3468b1cc0
Emanuel <emanuelnevesti@gmail.com>
2024-11-03 17:59:52 -0300

    implements localstorage

--- scripts.js
+++ scripts.js
@@ -15,18 +15,27 @@ const todosApp = {
         add: function () {
             if(!this.text.length){
                 this.alert = true;
-                console.log('deveria mostrar a msg');
                 return;
-                
             }
 
             this.todos.push({
                 text: this.text,
                 done: false
             });
+
             this.text = "";
             this.alert = false;
-        }
+
+            localStorage.setItem("todos",  JSON.stringify(this.todos));
+        },
     },
+    created() {
+        this.todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : this.todos;
+    },
+    updated(){
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+    }
 };
+
 Vue.createApp(todosApp).mount('#app');
+
